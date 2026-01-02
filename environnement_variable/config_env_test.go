a14c9ed3f9581754d385068be07c31f01968fe58
nitrax <jean.lejeune@flowbank.com>
2026-01-02 16:28:56 +0100

    Update environnement_variable package TU

--- environnement_variable/config_env_test.go
+++ environnement_variable/config_env_test.go
@@ -14,6 +14,18 @@ func TestConfigLoadSuccess(t *testing.T) {
 	assert.Equal(t, "test", EnvironmentVariable["test"])
 }
 
+func TestReloadEnv(t *testing.T) {
+	_ = os.Setenv("test", "test")
+	loadEnv()
+
+	assert.Equal(t, "test", EnvironmentVariable["test"])
+
+	_ = os.Setenv("test", "test_2")
+	ReloadEnv()
+
+	assert.Equal(t, "test_2", EnvironmentVariable["test"])
+}
+
 func TestConfigLoadFail(t *testing.T) {
 	assert.Equal(t, "", EnvironmentVariable["fail"])
 }
